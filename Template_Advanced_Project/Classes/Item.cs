f12aa18f99538b0948b65f2a0df63fcd9cfc32f9
Fratched <domenicobarille@gmail.com>
2025-11-20 18:15:55 -0500

    Question1 Completed

---
+++ Template_Advanced_Project/Classes/Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Advanced_Project.Classes
+{
+    public class Item
+    {
+
+        private string Name;
+        private string Id;
+
+        public string Name1 { get => Name; set => Name = value; }
+        public string Id1 { get => Id; set => Id = value; }
+
+
+        public Item(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return Name + Id;
+            
+
+        } 
+      
+    }
+}
